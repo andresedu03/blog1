ec88d4a2efd6cf1e4f3e6c8ff4a2575a06c17758
Andres Martinez <andreseduardo1206@gmail.com>
2020-11-13 19:23:36 -0300

    gp3

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,2 @@
 class Post < ApplicationRecord
-    validates :title, :image_url, :content, presence: true
-    before_save :censor
-
-    def censor
-        self.content = self.content.gsub("spoiler", " ")
-    end
 end
